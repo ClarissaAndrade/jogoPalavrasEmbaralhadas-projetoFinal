ffe163cb3c4f9ea00a728d1122b76e48b0669b44
Clarissa Andrade <clarissapa@gmail.com>
2019-12-07 12:40:57 -0300

    Elaborando Mecanica ainda

--- src/Principal.java
+++ src/Principal.java
@@ -5,16 +5,31 @@ public class Principal {
 	public static void main(String[] args) {
 	
 		
+	Scanner sc = new Scanner(System.in);
 		
 	BancoDePalavras teste = new BancoDePalavras();
-	String palavra = teste.retornaPalavra();
-	System.out.println(palavra);
-	
-	
 	FabricaEmbaralhadores fabrica = new FabricaEmbaralhadores();
-	Embaralhador emb = fabrica.retornaEmbaralhador(2);
+	FabricaMecanicaDoJogo fabricamecanica = new FabricaMecanicaDoJogo();
+	
+	
+	System.out.println("Bem vindo ao jogo de palavras embaralhadas!");
+	System.out.println("Será fornecida uma palavra embaralhada e você deve tentar descobrir qual é.");
+	System.out.println("Qual o level deseja: (1 - Fácil ; 2 - Difícil) Digite apenas o número:");
+	int level = sc.nextInt();
+	
+	MecanicaDoJogo mecanica = fabricamecanica.retornaMecanica(level);
+	
+	System.out.println("Qual é a seguinte palavra: ");
+	String palavra = teste.retornaPalavra();
+	Embaralhador emb = fabrica.retornaEmbaralhador(level);
 	String reversa = emb.embaralhar(palavra);
-	System.out.println(reversa);
+	
+	String resposta = sc.nextLine();
+	
+	mecanica.tentativa();
+	
+	
+	sc.close();
 
 	}
 
